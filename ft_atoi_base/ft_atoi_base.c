df804f87c15f731108b6a0ca6b9a296a9dbe0976
Nord <114178811+nordisss@users.noreply.github.com>
2024-01-24 10:10:30 +0100

    Update ft_atoi_base.c

--- ft_atoi_base/ft_atoi_base.c
+++ ft_atoi_base/ft_atoi_base.c
@@ -1,4 +1,53 @@
-int	ft_atoi_base(const char *str, int str_base)
+int ft_atoi_base(const char *str, int str_base)
+{
+	int i = 0;
+	int simb = 1;
+	int var = 0;
+	
+	if(str[i] == '-')
+	{
+		simb = -1;
+		i++;
+	}
+	while(str[i] != '\0')
+	{
+		var *= str_base;
+		if(str[i] >= '0' && str[i] <= '9')
+			var += str[i] - '0';
+		else if(str[i] >= 'A' && str[i] <= 'Z')
+			var += str[i] - '7';
+		else if(str[i] >= 'a' && str[i] <= 'z')
+			var += str[i] - 'W';
+		i++;
+	}
+	return(var * simb);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* int	ft_atoi_base(const char *str, int str_base)
 {
 	int	i = 0;
 	int	sign = 1;
@@ -22,3 +71,4 @@ int	ft_atoi_base(const char *str, int str_base)
 	}
 	return (result * sign);
 }
+*/
